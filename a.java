40c9a0946ca0b5fa3778e0032fdad9b2065879ef
Avi_kaur <avikashyap620@yahoo.com>
2015-08-04 17:47:06 +0530

    a.java

---
+++ a.java
@@ -0,0 +1,20 @@
+class a
+{
+public static void main(String args[])
+{
+System.out.println("the pattern is :");
+for(int i=0;i<5;i++)
+{
+for(int j=0;j<5;j++)
+{
+if(j<=i)
+{
+System.out.print("*");
+}
+else
+{
+System.out.println("");
+}}
+System.out.println();
+}}}
+
